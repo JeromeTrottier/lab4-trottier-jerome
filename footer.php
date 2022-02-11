8ea019af0bc1fbecbfb246549f06492ff4f5d999
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-02-11 16:02:40 -0500

    Barre de recherche

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
     <h2 class="footer__titre">Un magnifique footer</h2>
     <p class="footer__presentation">Un site web fait à l'aide de wordpress en TIM</p>
     <h3 class="footer__author">Fait par Jérôme Trottier</h3>
+    <?php wp_nav_menu(array("menu" => "simple")) ?>
 </footer>
 </body>
 
